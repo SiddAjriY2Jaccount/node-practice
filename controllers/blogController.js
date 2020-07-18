7a2b9fd82b2788fedb07b23476f26a267a563df2
Siddhanth J Ajri <y2jsiddajri@gmail.com>
2020-07-19 04:43:39 +0530

    MVC approach: added controller functions

---
+++ controllers/blogController.js
@@ -0,0 +1,61 @@
+const Blog = require('../models/blog');
+// naming convention, same as MDN: blog_index, blog_create_post, blog_create_get, blog_details, blog_delete 
+
+const blog_index = (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+      .then((result) => {
+        res.render('index', { blogs: result, title: 'All blogs' });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+}
+
+const blog_create_post = (req, res) => {
+    //console.log(req.body);
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+        res.redirect('/blogs');
+        })
+        .catch((err) => {
+        console.log(err);
+        })
+} 
+
+const blog_create_get = (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+}
+
+const blog_details = (req, res) => {
+    const id = req.params.id;   //to fetch specific route param as mentioned above (Eg- :id)
+    Blog.findById(id)
+        .then(result => {
+        res.render('details', { blog: result, title: result.title });
+        })
+        .catch(err => {
+        console.log(err);
+        })
+}
+
+const blog_delete = (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+        .then(result => {
+        // cannot render here because this was an AJAX request, so response needs to be JSON encoded
+        res.json({ redirect: '/blogs' });
+        })
+        .catch(err => {
+        console.log(err)
+        });
+}
+
+module.exports = {
+      blog_index,
+      blog_create_post, 
+      blog_create_get,
+      blog_details,
+      blog_delete
+  }
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,64 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController')
 
 // Blogs -> Routes
-router.get('/', (req, res) => {
-    Blog.find().sort({ createdAt: -1 })
-      .then((result) => {
-        res.render('index', { blogs: result, title: 'All blogs' });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  });
+router.get('/', blogController.blog_index);
   
 // POST request to create a blog, hit submit and then redirect to /blogs page
-router.post('/', (req, res) => {
-    //console.log(req.body);
-    const blog = new Blog(req.body);
-
-    blog.save()
-        .then((result) => {
-        res.redirect('/blogs');
-        })
-        .catch((err) => {
-        console.log(err);
-        })
-})
+router.post('/', blogController.blog_create_post)
 
 // render form to create blog 
-router.get('/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-});
+router.get('/create', blogController.blog_create_get);
 
-
-// GET request to access specific blog using route params
-router.get('/:id', (req, res) => {
-    const id = req.params.id;   //to fetch specific route param as mentioned above (Eg- :id)
-    Blog.findById(id)
-        .then(result => {
-        res.render('details', { blog: result, title: result.title });
-        })
-        .catch(err => {
-        console.log(err);
-        })
-})
+// GET request to access specific blog's details using route params
+router.get('/:id', blogController.blog_details)
 
 // Handle DELETE request from frontend to delete a blog post
-router.delete('/:id', (req, res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then(result => {
-        // cannot render here because this was an AJAX request, so response needs to be JSON encoded
-        res.json({ redirect: '/blogs' });
-        })
-        .catch(err => {
-        console.log(err)
-        });
-})
-
+router.delete('/:id', blogController.blog_delete)
 
 // export for invoking in app.js
 module.exports = router;
